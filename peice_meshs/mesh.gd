bdea0c3b24128f230f8fb67d60607ba9b5708885
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-10-28 18:53:18 -0300

    added some adcanced meshs for fun.

--- peice_meshs/mesh.gd
+++ peice_meshs/mesh.gd
@@ -1,13 +1,17 @@
-extends MeshInstance3D
+extends Node3D
 
 @export var is_light: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if is_light:
-		material_override = preload("res://peice_meshs/white_piece_material.tres")
-	else:
-		material_override = preload("res://peice_meshs/black_piece_material.tres")
+	var direction = Vector3(1, 0, 0)
+	for child in get_children():
+		if is_light:
+			child.material_override = preload("res://peice_meshs/white_piece_material.tres")
+			child.rotation_degrees.y -= 90
+		else:
+			child.material_override = preload("res://peice_meshs/black_piece_material.tres")
+			child.rotation_degrees.y += 90
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
