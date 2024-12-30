bff0de829cc310ff790327dbe487dec991bdd239
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-12-30 14:34:50 -0400

    I removed the outline for now will work on it on a new branch.

--- peice_meshs/mesh.gd
+++ peice_meshs/mesh.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
 @export var is_light: bool
+#@export var outline_material: Material = preload(("res://outline/outline.tres"))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var direction = Vector3(1, 0, 0)
+	#outline_material.set_shader_parameter("outline_color", Color(255 / 255.0, 81 / 255.0, 0 / 255.0))
+	#outline_material.set_shader_parameter("outline_width", 3)
 	for child in get_children():
+		#child.material_overlay = outline_material
 		if is_light:
 			child.material_override = preload("res://peice_meshs/white_piece_material.tres")
 			child.rotation_degrees.y -= 90
